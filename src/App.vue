60827ba435ab5d34552aa7f6e8f4ee19106fdab9
u2knox <ttuleubayev@alseco.kz>
2024-02-08 20:34:25 +0600

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,6 @@ import { ref, computed } from "vue";
 const password = ref("test");
 const age = ref(0);
 
-// 1. Но и проверял чтобы в пароле были и числа и 
-// заaглавные буквы и спец символы( .?!$#& )
-// 2. Сделать инпут с вводом возразта и давать ошибку
-// если возраст юзер будет меньше 15
-
-/[\?\.\!\$\#\&]/
-
 const isValidPassword = computed(() => {
   console.log('isValidPassword called')
   if (password.value.length < 7) return false;
